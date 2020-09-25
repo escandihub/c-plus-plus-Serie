4a00763730535f874982d40a2950ad64839c80d9
Escandon <aleskndon@hotmail.com>
2020-09-25 12:37:38 -0500

    encapsulation example

--- clasess.cpp
+++ clasess.cpp
@@ -2,19 +2,38 @@
 using namespace std;
 
 class Demon{
-  public:
+  private:
     string Name;
     string Hability;
     string Sin;
+    bool IsOnVacation;
 
+public:
     Demon(string name, string hability, string sin){
       Name = name;
       Hability = hability;
       Sin = sin;
+      IsOnVacation = false;
     }
 
     void getCharacter(){
       cout << "This is " << Name << " has the hability to " << Hability << endl;
+    }
+    void onVacation(){
+      cout << Name << " On vacation"<<endl;
+      IsOnVacation = true;
+    }
+    void notOnVation(){
+      cout << Name << " On the job "<<endl;
+      IsOnVacation = false;
+    }
+    void checkDemon(){
+      if(IsOnVacation){
+        cout << Name << " is on vacation " <<endl;
+      }
+      else{
+        cout << Name << " is working " <<endl;
+      }
     }
 };
 
@@ -22,6 +41,10 @@ int main(){
   Demon luci("Lucifer", "Kill people", "gula");
   Demon bafomed("Bafoment", "be cool", "evil");
 
-  luci.getCharacter();
-  bafomed.getCharacter();
+  luci.checkDemon();
+  luci.onVacation();
+  luci.checkDemon();
+  luci.notOnVation()
+  luci.checkDemon();
+  
 }
